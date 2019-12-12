352cb2f84a51d6c7b24f7dc09556e36abd2956a3
HugoHutri <Hugo.Hutri@student.lut.fi>
2019-12-13 00:09:27 +0200

    Update axios.js

--- client/src/axios.js
+++ client/src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: "http://nodejs-mongo-backend-vuori.rahtiapp.fi/"
+    baseURL: "http://api-vuori.rahtiapp.fi/"
     //baseURL: "http://localhost:9000/"
 });
 
